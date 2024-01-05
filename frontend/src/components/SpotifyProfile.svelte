8b6709bba886f722f2a0fb23ddd478c8d3baca90
Petteri Mattila <petterimattila@protonmail.com>
2024-01-05 17:50:27 +0200

    Working user authentication

---
+++ frontend/src/components/SpotifyProfile.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import { makeApiRequest } from "../lib/spotify"
+
+    let profile: string | null = null
+
+    async function getProfile() {
+        const profileRes = await makeApiRequest("/spotify/profile")
+        profile = profileRes
+        console.log(profile)
+    }
+
+    getProfile()
+</script>
+
+<div>
+    Profile
+</div>
